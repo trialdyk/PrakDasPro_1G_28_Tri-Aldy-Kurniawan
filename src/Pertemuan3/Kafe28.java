ea793c45a715faf02104b7eb2c5f29f71a9997ca
trialdyk <aldykuniawan94@gmail.com>
2024-09-19 19:43:53 +0700

    P3_Percobaan2

---
+++ src/Pertemuan3/Kafe28.java
@@ -0,0 +1,31 @@
+package Pertemuan3;
+import java.util.Scanner;
+
+public class Kafe28 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       boolean keanggotaan;
+       int jmlKopi, jmlTeh,jmlRoti;
+       double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
+       float diskon = 10 / 100f;
+
+
+       System.out.print("Masukkan Keanggotaan (true/false): ");
+       keanggotaan = input.nextBoolean();
+       System.out.print("Masukkan jumlah pembelian kopi : ");
+       jmlKopi = input.nextInt();
+       System.out.print("Masukkan jumlah pembelian teh : ");
+       jmlTeh = input.nextInt();
+       System.out.print("Masukkan jumlah pembelian roti : ");
+       jmlRoti = input.nextInt();
+       
+       double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+
+       double nominalBayar = totalHarga - (diskon * totalHarga);
+
+       System.out.println("Keanggotaan : " + keanggotaan);
+       System.out.println("Item Pembelian : " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
+       System.out.println("Nominal Bayar : " + nominalBayar);
+    }
+}
