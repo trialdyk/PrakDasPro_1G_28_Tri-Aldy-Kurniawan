e65ee9587964b9326d873126af12a97f3658008b
trialdyk <aldykuniawan94@gmail.com>
2024-09-19 19:21:17 +0700

    P3_Percobaan1

---
+++ src/Pertemuan2/P2_Percobaan3_Pertanyaan1_28.java
@@ -0,0 +1,29 @@
+package Pertemuan2;
+
+/**
+ * P2_Percobaan3_Pertanyaan1_28
+ */
+public class P2_Percobaan3_Pertanyaan1_28 {
+
+    public static void main(String[] args) {
+        System.out.println("\n\n\t\t\t\t\tKARTU RENCANA STUDI (KRS)\n\n");
+        System.out.println("NIM\t\t\t: 244107020098");
+        System.out.println("NAMA\t\t\t: TRI ALDY KURNIAWAN");
+        System.out.println("PROGRAM STUDI\t\t: Diploma IV Teknik Informatika");
+        System.out.println("KELAS\t\t\t: 1G\n");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("|\tKODE MK\t\t|\t\tMATA KULIAH\t\t|\tSKS\t|\tJAM\t|");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("|\tRTI241001\t| Pancasila\t\t\t\t|\t2\t|\t2\t|");
+        System.out.println("|\tRTI241002\t| Konsep Teknologi Informasi\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRTI241003\t| Critical Thinking dan Problem Solving\t|\t2\t|\t4\t|");
+        System.out.println("|\tRTI241004\t| Matematika Dasar\t\t\t|\t3\t|\t6\t|");
+        System.out.println("|\tRTI241005\t| Rekayasa Perangkat Lunak\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRTI241006\t| Dasar Pemrograman\t\t\t|\t2\t|\t4\t|");
+        System.out.println("|\tRTI241007\t| Praktikum Dasar Pemrograman\t\t|\t2\t|\t6\t|");
+        System.out.println("|\tRTI241008\t| Keselamatan dan Kesehatan Kerja\t|\t2\t|\t4\t|");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+        System.out.println("|\t\t\t|\t\tJUMLAH\t\t\t|\t18\t|\t34\t|");
+        System.out.println("-------------------------------------------------------------------------------------------------");
+    }
+}
---
+++ src/Pertemuan3/Siakad28.java
@@ -0,0 +1,33 @@
+package Pertemuan3;
+import java.util.Scanner;
+
+public class Siakad28 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        String nama, nim;
+        char kelas;
+        byte absen;
+        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+
+        System.out.print("Masukkan nama: ");
+        nama = input.nextLine();
+        System.out.print("Masukkan nim: ");
+        nim = input.nextLine();
+        System.out.print("Masukkan kelas: ");
+        kelas = input.nextLine().charAt(0);
+        System.out.print("Masukkan absen: ");
+        absen = input.nextByte();
+        
+
+        System.out.print("Masukkan nilai kuis: ");
+        nilaiKuis = input.nextDouble();
+        System.out.print("Masukkan nilai tugas: ");
+        nilaiTugas = input.nextDouble();
+        System.out.print("Masukkan nilai ujian: ");
+        nilaiUjian = input.nextDouble();
+
+        
+        
+    }
+}
