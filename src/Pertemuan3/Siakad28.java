82ba4f937368e28b20fca428d421a0703c711912
trialdyk <aldykuniawan94@gmail.com>
2024-09-19 19:31:44 +0700

    P3_Percobaan1_Pertanyaan06

--- src/Pertemuan3/Siakad28.java
+++ src/Pertemuan3/Siakad28.java
@@ -7,7 +7,7 @@ public class Siakad28 {
 
         String nama, nim, kelas;
         byte absen;
-        double nilaiKuis, nilaiTugas, nilaiUjian, nilaiAkhir;
+        double nilaiKuis, nilaiTugas, NilaiUTS,nilaiUAS, nilaiAkhir;
 
         System.out.print("Masukkan nama: ");
         nama = input.nextLine();
@@ -23,10 +23,15 @@ public class Siakad28 {
         nilaiKuis = input.nextDouble();
         System.out.print("Masukkan nilai tugas: ");
         nilaiTugas = input.nextDouble();
-        System.out.print("Masukkan nilai ujian: ");
-        nilaiUjian = input.nextDouble();
+        System.out.println("Masukkan nilai UTS: ");
+        NilaiUTS = input.nextDouble();
+        System.out.println("Masukkan nilai UAS: ");
+        nilaiUAS = input.nextDouble();
 
-        
-        
+        nilaiAkhir = (nilaiKuis * 0.2) + (nilaiTugas * 0.15) + (NilaiUTS * 0.3) + (nilaiUAS * 0.3);
+
+        System.out.println("Nama: " + nama +"Nim :" +nim);
+        System.out.println("Kelas: " + kelas +"Absen :" +absen);
+        System.out.println("Nilai Akhir : " +nilaiAkhir);   
     }
 }
