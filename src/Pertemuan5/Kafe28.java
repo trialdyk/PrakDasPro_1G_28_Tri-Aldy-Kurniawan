0893e7053880c208188442b3c170c8a07834490f
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-10-03 20:53:30 +0700

    P5_Tugas2

---
+++ src/Pertemuan5/Kafe28.java
@@ -0,0 +1,37 @@
+package Pertemuan5;
+import java.util.Scanner;
+
+public class Kafe28 {
+    public static void main(String[] args) {
+       Scanner input = new Scanner(System.in);
+
+       boolean keanggotaan;
+       int jmlKopi, jmlTeh,jmlRoti;
+       double hargaKopi = 12000.0, hargaTeh = 7000.0, hargaRoti = 20000.0;
+       float diskon = 10 / 100;
+
+
+       System.out.print("Masukkan Keanggotaan (true/false): ");
+       keanggotaan = input.nextBoolean();
+       System.out.print("Masukkan jumlah pembelian kopi : ");
+       jmlKopi = input.nextInt();
+       System.out.print("Masukkan jumlah pembelian teh : ");
+       jmlTeh = input.nextInt();
+       System.out.print("Masukkan jumlah pembelian roti : ");
+       jmlRoti = input.nextInt();
+       
+       double totalHarga = (jmlKopi * hargaKopi) + (jmlTeh * hargaTeh) + (jmlRoti * hargaRoti);
+       byte totalByte = (byte) totalHarga;
+
+       if(!keanggotaan) diskon = 0;
+       
+       double nominalBayar = totalHarga - (diskon * totalHarga);
+       int nominalInt = (int) nominalBayar;
+
+       System.out.println("Keanggotaan : " + keanggotaan);
+       System.out.println("Item Pembelian : " + jmlKopi + " kopi, " + jmlTeh + " teh, " + jmlRoti + " roti");
+       System.out.println("Nominal Bayar : " + nominalBayar);
+       System.out.println("Nominal Int : " + nominalInt);
+       System.out.println("Total Byte : " + totalByte);
+    }
+}
