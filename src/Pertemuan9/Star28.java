32cc5adb5704118ddba4514ca4c9c765bbfeb1dd
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-10-25 12:53:23 +0700

    P9_Percobaan1

---
+++ src/Pertemuan9/Star28.java
@@ -0,0 +1,14 @@
+package Pertemuan9;
+import java.util.Scanner;
+public class Star28 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Masukkan nilai N : ");
+        int n = sc.nextInt();
+
+        for(int i = 1; i <= n; i++){
+            System.out.print("*");
+        }
+    }
+}
