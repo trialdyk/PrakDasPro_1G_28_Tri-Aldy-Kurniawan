5e35bc248d381df86320ee72a106756f9f8ef2df
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-11-01 14:42:46 +0700

    P10_Percobaan1

---
+++ src/Pertemuan10/ArrayBilangan28.java
@@ -0,0 +1,12 @@
+package Pertemuan10;
+public class ArrayBilangan28 {
+    public static void main(String[] args) {
+        int[] bil = {5, 13, -7, 17};
+
+
+        for (int i = 0; i < 4; i++) {
+            System.out.println(bil[i]);
+        }
+
+    }
+}
