6c370b079244fea92c93d66740953d5e433b18db
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-11-14 22:50:08 +0700

    P11_Percobaan1_Pertanyaan07

--- src/Pertemuan11/Bioskop28.java
+++ src/Pertemuan11/Bioskop28.java
@@ -26,5 +26,11 @@ public class Bioskop28 {
         for (String[] strings : penonton) {
             System.out.println("Panjang Baris : "+strings.length);
         }
+
+        System.out.println("Penonton pada baris ke 3");
+
+        for (int i = 0; i < penonton[2].length; i++) {
+            System.out.println(penonton[2][i]);
+        }
     }
 }
