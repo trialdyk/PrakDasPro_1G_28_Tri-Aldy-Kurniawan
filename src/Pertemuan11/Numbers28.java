b2f19668b19f246fb3d590d39a9a385febefe7b3
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-11-14 23:25:51 +0700

    P11_Percobaan3

---
+++ src/Pertemuan11/Numbers28.java
@@ -0,0 +1,22 @@
+package Pertemuan11;
+
+import java.util.Arrays;
+
+public class Numbers28 {
+    public static void main(String[] args) {
+        int[][] myNumbers = new int[3][];
+
+        myNumbers[0] =  new int[5]; 
+        myNumbers[1] =  new int[3]; 
+        myNumbers[2] =  new int[1]; 
+
+
+        for(int i = 0; i < myNumbers.length; i++) {
+            System.out.println(Arrays.toString(myNumbers[i]));
+        }
+
+        for(int i = 0; i < myNumbers.length; i++) {
+            System.out.println("Panjang Baris ke "+i+" = "+myNumbers[i].length);
+        }
+    }
+}
