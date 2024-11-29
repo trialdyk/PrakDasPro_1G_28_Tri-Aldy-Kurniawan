7743452ee3c23eef1c23c197a788ff0c8552cd84
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-11-29 14:16:35 +0700

    P13_Percobaan4_Pertanyaan02

--- src/Pertemuan13/PengunjungKafe28.java
+++ src/Pertemuan13/PengunjungKafe28.java
@@ -7,10 +7,8 @@ public class PengunjungKafe28 {
 
     public static void daftarPengunjung(String... namaPengunjung){
         System.out.println("Daftar Nama Pengunjung");
-
-        for(int i = 1; i < namaPengunjung.length; i++){
-            System.out.println(i+1 + ". " + namaPengunjung[i]);
+        for(String nama : namaPengunjung){
+            System.out.println(nama);
         }
-
     }
 }
