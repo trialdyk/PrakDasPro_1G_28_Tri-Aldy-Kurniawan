384aac402636ced9c0777aff4ea6808c4a4dd0b3
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2024-11-29 13:25:40 +0700

    P13_Percobaan1

---
+++ src/Pertemuan13/Kafe28.java
@@ -0,0 +1,19 @@
+package Pertemuan13;
+
+public class Kafe28 {
+    public static void main(String[] args) {
+        Menu();
+    }
+
+    public static void Menu() {
+        System.out.println("===== Menu Resto Kafe =====");
+        System.out.println("1. Kopi Hitam - Rp. 15,000");
+        System.out.println("2. Cappucino - Rp. 20,000");
+        System.out.println("3. Latte - Rp. 22,000");
+        System.out.println("4. Teh Tarik - Rp. 12,000");
+        System.out.println("5. Roti Bakar - Rp. 10,000");
+        System.out.println("6. Mie Goreng - Rp. 18,000");
+        System.out.println("===========================");
+        System.out.println("Silakan pilih menu yang anda inginkan.");
+    }
+}
