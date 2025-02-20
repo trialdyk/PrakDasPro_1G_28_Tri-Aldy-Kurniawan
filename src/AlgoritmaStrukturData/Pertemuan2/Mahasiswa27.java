46fb6b41a09dbbefdb4ddbf7c28b846e82a19840
Tri Aldy Kurniawan <122498697+trialdyk@users.noreply.github.com>
2025-02-20 08:12:08 +0700

    P2_Percobaan1

---
+++ src/AlgoritmaStrukturData/Pertemuan2/Mahasiswa27.java
@@ -0,0 +1,34 @@
+package AlgoritmaStrukturData.Pertemuan2;
+public class Mahasiswa27 {
+    String nama;
+    String nim;
+    String kelas;
+    double ipk;
+    void tampilkanInformasi() {
+        System.out.println("Nama : " + nama);
+        System.out.println("NIM : " + nim);
+        System.out.println("IPK : " + ipk);
+        System.out.println("Kelas : " + kelas);
+    }
+    void ubahKelas(String kelasBaru) {
+        kelas = kelasBaru;
+    }
+    void ubahIPK(double ipkBaru) {
+        if(ipkBaru >= 0 && ipkBaru <= 4.0){
+            ipk = ipkBaru;
+        }else{
+            System.out.println("IPK Tidak Valid, harus antara 0.0 sampai 4.0");
+        }
+    }
+    String nilaiKinerja(){
+        if(ipk >= 3.5){
+            return "Kinerja sangat baik";
+        }else if(ipk >= 3.0){
+            return "Kinerja baik";
+        }else if(ipk >= 2){
+            return "Kinerja cukup";
+        }else{
+            return "Kinerja kurang";
+        }
+    }
+}
